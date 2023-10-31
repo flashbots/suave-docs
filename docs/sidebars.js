13ba35a8c49fcb41080b68bfb36c3008e7ff0178
Chris Hager <chris@linuxuser.at>
2023-10-31 12:11:40 +0100

    precompiles as own section

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -32,7 +32,13 @@ module.exports = {
       collapsed: false,
       link: { type: 'doc', id: 'technical/README' },
       items: [
-        'technical/specs/rigil/README', 'technical/specs/rigil/suave-chain', 'technical/specs/rigil/mevm', 'technical/specs/rigil/confidential-data-store', 'technical/specs/rigil/kettle', 'technical/specs/rigil/bridge',
+        'technical/specs/rigil/README',
+        'technical/specs/rigil/suave-chain',
+        'technical/specs/rigil/mevm',
+        'technical/specs/rigil/precompiles',
+        'technical/specs/rigil/confidential-data-store',
+        'technical/specs/rigil/kettle',
+        'technical/specs/rigil/bridge',
       ],
     },
     // {
