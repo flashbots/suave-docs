5c499d08c5615e1ab176031520b4bc145da36aa9
andytudhope <13001517+andytudhope@users.noreply.github.com>
2023-11-09 22:20:42 +0200

    Adds linting rules, with a few cheats

---
+++ .stylelintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind',
+          'apply',
+          'variants',
+          'responsive',
+          'screen',
+        ],
+      },
+    ],
+    'declaration-block-trailing-semicolon': null,
+    'no-descending-specificity': null,
+    'selector-class-pattern': null,
+  },
+};
--- src/theme/Layout/index.tsx
+++ src/theme/Layout/index.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+/**
+ * Copyright (c) Flashbots Ltd. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 import Layout from '@theme-original/Layout';
 import type LayoutType from '@theme/Layout';
 import type {WrapperProps} from '@docusaurus/types';
-import { Analytics } from '@vercel/analytics/react';
+import {Analytics} from '@vercel/analytics/react';
+
 type Props = WrapperProps<typeof LayoutType>;
 
 export default function LayoutWrapper(props: Props): JSX.Element {
