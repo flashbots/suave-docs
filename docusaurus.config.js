b33ee1200101fec8b99cb3505e9732675faa00f2
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2023-11-22 15:23:08 -0800

    update viem code examples

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,7 +51,7 @@ module.exports = async function createConfigAsync() {
         prism: {
           theme: lightTheme,
           darkTheme,
-          additionalLanguages: ['solidity'],
+          additionalLanguages: ['solidity', 'bash'],
         },
         docs: {
           sidebar: {
