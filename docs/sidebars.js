8a791819907dcb530045908a26c54c903553d7c4
andytudhope <13001517+andytudhope@users.noreply.github.com>
2024-03-29 16:27:42 +0200

    CDS + External calls beginning

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -38,6 +38,8 @@ module.exports = {
         'tutorials/run-suave',
         'tutorials/deploy-contracts',
         'tutorials/onchain-offchain',
+        'tutorials/confidential-store',
+        'tutorials/external-call',
         'tutorials/build-suapp-webapp',
         'tutorials/confidential-compute-requests',
         'tutorials/create-precompiles'
